461dc4fdc0be8716615c46e7d9bdc0295e5ebd34
Giordano Scalzo <giordano.scalzo@thetrainline.com>
2019-03-26 21:54:24 +0000

    Renamed to ARKitDartboard

---
+++ Chapter-03-Detecting-Objects/ARKitDartBoard/ARKitDartboard/Dartboard.swift
@@ -0,0 +1,35 @@
+//
+//  Dartboard.swift
+//  ARKitDartboard
+//
+//  Created by Giordano Scalzo on 26/03/2019.
+//  Copyright © 2019 Giordano Scalzo. All rights reserved.
+//
+
+import Foundation
+import ARKit
+import SceneKit
+
+class Dartboard: SCNNode {
+    init(at position: SCNVector3, eulerAngles: SCNVector3) {
+        super.init()
+
+        let planeGeometry = SCNPlane(width: 0.5, height: 0.5)
+        planeGeometry.cornerRadius = planeGeometry.width / 2
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "target")
+        planeGeometry.materials = [material]
+
+        let node = SCNNode(geometry: planeGeometry)
+        node.position = position
+        node.eulerAngles = eulerAngles
+        node.eulerAngles.x = 0
+
+        addChildNode(node)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
